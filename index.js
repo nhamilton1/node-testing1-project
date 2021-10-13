ad08e3c13e1ffb11e34dbb39c12013144e89b09d
nhamilton1 <nickh3592@gmail.com>
2021-10-13 14:29:07 -0400

    setting up

--- index.js
+++ index.js
@@ -8,6 +8,14 @@
  */
 function trimProperties(obj) {
   // ✨ implement
+  const copyObj = []
+  const trimmed = JSON.stringify(obj, (key, value) => {
+    if (typeof value === 'string') {
+      return value.trim();
+    }
+    return value;
+  });
+  return JSON.parse(trimmed);
 }
 
 /**
@@ -20,6 +28,13 @@ function trimProperties(obj) {
  */
 function trimPropertiesMutation(obj) {
   // ✨ implement
+  const trimmed = JSON.stringify(obj, (key, value) => {
+    if (typeof value === 'string') {
+      return value.trim();
+    }
+    return value;
+  });
+  return JSON.parse(trimmed);
 }
 
 /**
--- index.test.js
+++ index.test.js
@@ -8,7 +8,10 @@ describe('[Exercise 1] trimProperties', () => {
     const actual = utils.trimProperties(input)
     expect(actual).toEqual(expected)
   })
-  // test('[2] returns a copy, leaving the original object intact', () => {})
+  test('[2] returns a copy, leaving the original object intact', () => {
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    expect(input).toEqual(input)
+  })
 })
 
 describe('[Exercise 2] trimPropertiesMutation', () => {
